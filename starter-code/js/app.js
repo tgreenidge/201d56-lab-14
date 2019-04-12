603b9e4b3dc2dd32914975e1da9328e37749b520
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-04-12 09:04:38 -0700

    create functions in app.js

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -8,15 +8,24 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  this.product = product;
+  this.quantity = quantity;
+  this.items(this);
 };
 
-Cart.prototype.saveToLocalStorage = function() {
+Cart.prototype.saveToLocalStorage = function(item) {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('contents', JSON.stringify(items));
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  var items = JSON.parse(localStorage.getItem('contents'));
+  var itemIndex = items.indexOf(item);
+  if(itemIndex !== -1){
+    items.splice(itemIndex, 1);
+  }
 };
 
 var CartItem = function(product, quantity) {
