8af12497ba1385dcdf7d0d32300104f814463bc1
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-04-12 09:23:07 -0700

    modify removeIem funtion to use the cart from local storage

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -21,7 +21,7 @@ Cart.prototype.saveToLocalStorage = function() {
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
-  var items = JSON.parse(localStorage.getItem('contents'));
+  var items = JSON.parse(localStorage.getItem('cart'));
   var itemIndex = items.indexOf(item);
   if(itemIndex !== -1){
     items.splice(itemIndex, 1);
